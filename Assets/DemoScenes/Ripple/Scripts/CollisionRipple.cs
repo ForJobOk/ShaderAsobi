98b7774d8b390dfc3b81d140b69e0b47aa34a1fd
ONOUEKENTO <kentou1506@gmail.com>
2021-02-23 03:12:06 +0900

    Modify CollisionRipple

--- Assets/DemoScenes/Ripple/Scripts/CollisionRipple.cs
+++ Assets/DemoScenes/Ripple/Scripts/CollisionRipple.cs
@@ -7,9 +7,12 @@ public class CollisionRipple : MonoBehaviour
 {
     [SerializeField] private CustomRenderTexture _customRenderTexture;
     [SerializeField, Range(0.01f, 0.05f)] private float _ripppleSize = 0.01f;
+    [SerializeField,Range(0.01f, 0.1f)] private float _ray_distance = 0.05f;
+    [SerializeField] private Transform rayObj;
     [SerializeField] private int iterationPerFrame = 5;
-
+    
     private CustomRenderTextureUpdateZone _defaultZone;
+    private Texture2D _texture2D;
 
     private void Start()
     {
@@ -32,27 +35,30 @@ public class CollisionRipple : MonoBehaviour
     {
         //クリック時のUpdateZoneがクリック後も適応された状態にならないように一度消去する
         _customRenderTexture.ClearUpdateZones();
+        UpdateZonesClickArea();
         //更新したいフレーム数を指定して更新
         _customRenderTexture.Update(iterationPerFrame);
     }
 
-    private void OnTriggerStay(Collider other)
+    private void UpdateZonesClickArea()
     {
-        Debug.Log("Enter");
-        Vector3 hitPos = other.ClosestPointOnBounds(this.transform.position);
+        Ray ray = new Ray(rayObj.position, -rayObj.up);//下へレイを出す
         
-        //クリック時に使用するUpdateZone
-        //クリックした箇所を更新の原点とする
-        //使用するパスもクリック用に変更
-        var clickZone = new CustomRenderTextureUpdateZone
+        if (Physics.Raycast(ray, out var hit))
         {
-            needSwap = true,
-            passIndex = 1,
-            rotation = 0f,
-            updateZoneCenter = new Vector2(hitPos.x, 1f - hitPos.y),
-            updateZoneSize = new Vector2(_ripppleSize, _ripppleSize)
-        };
+            //クリック時に使用するUpdateZone
+            //クリックした箇所を更新の原点とする
+            //使用するパスもクリック用に変更
+            var clickZone = new CustomRenderTextureUpdateZone
+            {
+                needSwap = true,
+                passIndex = 1,
+                rotation = 0f,
+                updateZoneCenter = new Vector2(hit.textureCoord.x, 1f - hit.textureCoord.y),
+                updateZoneSize = new Vector2(_ripppleSize, _ripppleSize)
+            };
 
-        _customRenderTexture.SetUpdateZones(new CustomRenderTextureUpdateZone[] {_defaultZone, clickZone});
+            _customRenderTexture.SetUpdateZones(new CustomRenderTextureUpdateZone[] {_defaultZone, clickZone});
+        }
     }
 }
