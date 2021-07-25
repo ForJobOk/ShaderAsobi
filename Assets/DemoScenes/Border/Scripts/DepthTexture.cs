6b08d2b069f842d8657657db3da838c1f8626887
ONOUEKENTO <kentou1506@gmail.com>
2021-07-25 19:09:03 +0900

    深度テクスチャ生成用コード

---
+++ Assets/DemoScenes/Border/Scripts/DepthTexture.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class DepthTexture : MonoBehaviour 
+{
+  private Camera cam;
+
+  void Start () 
+  {
+    cam = GetComponent<Camera>();
+    cam.depthTextureMode = DepthTextureMode.Depth;
+  }
+}
